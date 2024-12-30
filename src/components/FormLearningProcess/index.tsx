e43e55178511479e6a6e86b82fa5a896db4cdf3a
HieuNguyen67 <hieu012846@gmail.com>
2024-12-30 20:17:50 +0700

    add form learning process

---
+++ src/components/FormLearningProcess/index.tsx
@@ -0,0 +1,164 @@
+import React, { useState } from "react";
+import { db } from "../../firebaseConfig";
+import { collection, addDoc } from "firebase/firestore";
+import {
+  Input,
+  Button,
+  Form,
+  notification,
+  Col,
+  Row,
+  Modal,
+  Select,
+} from "antd";
+import { RiSendPlaneFill } from "react-icons/ri";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+
+const FormLearningProcess: React.FC = () => {
+  const [form] = Form.useForm();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [docId, setDocId] = useState("");
+  const user = useSelector((state: RootState) => state.auth);
+
+  const handleSubmit = async (values: {
+    group: string;
+    linkFile: string;
+    content: string;
+  }) => {
+    try {
+      const docRef = await addDoc(collection(db, "learning-process"), {
+        group: values.group,
+        linkFile: values.linkFile,
+        content: values.content,
+        createdAt: new Date(),
+        userId: user.id,
+      });
+
+      setDocId(docRef.id);
+      setIsModalVisible(true);
+      form.resetFields();
+    } catch (error) {
+      notification.error({
+        message: "Error",
+        description: "Failed to add company.",
+      });
+    }
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <div>
+      <Form
+        form={form}
+        layout="vertical"
+        onFinish={handleSubmit}
+        initialValues={{
+          group: "",
+          linkFile: "",
+          content: "",
+        }}
+      >
+        <Row>
+          <Col lg={24} xs={24}>
+            <Form.Item
+              label="Chọn lớp / Chọn nhóm thực tập"
+              name="group"
+              rules={[
+                {
+                  required: true,
+                  message: "Vui lòng Chọn lớp / Chọn nhóm thực tập",
+                },
+              ]}
+              style={{ margin: "16px", width: "50%" }}
+            >
+              <Select placeholder="Chọn lớp / Chọn nhóm thực tập">
+                <Select.Option value="Internship_Frontend_11/2024">
+                  Internship_Frontend_11/2024
+                </Select.Option>
+                <Select.Option value="Internship_Frontend_09/2024">
+                  Internship_Frontend_09/2023
+                </Select.Option>
+                <Select.Option value="Internship_Frontend_06/2024">
+                  Internship_Frontend_06/2023
+                </Select.Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col lg={24} xs={24}>
+            <Form.Item
+              label="Link file"
+              name="linkFile"
+              rules={[{ required: true, message: "Vui lòng nhập Link file" }]}
+              style={{ margin: "16px", width: "50%" }}
+            >
+              <Input placeholder="Nhập Link file" />
+            </Form.Item>
+          </Col>
+          <Col lg={24} xs={24}>
+            <Form.Item
+              label="Nội dung báo cáo"
+              name="content"
+              rules={[
+                { required: true, message: "Vui lòng nhập Nội dung báo cáo" },
+              ]}
+              style={{ margin: "16px", width: "50%" }}
+            >
+              <Input.TextArea placeholder="Nhập Nội dung báo cáo" />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Form.Item>
+          <Button
+            style={{
+              backgroundColor: "rgb(255, 90, 0)",
+              color: "white",
+              fontSize: "15px",
+              margin: "16px",
+            }}
+            htmlType="submit"
+          >
+            Gửi <RiSendPlaneFill />
+          </Button>
+        </Form.Item>
+      </Form>
+
+      <Modal
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleOk}
+        okText="OK"
+        cancelButtonProps={{ style: { display: "none" } }}
+        okButtonProps={{ style: { display: "none" } }}
+      >
+        <h1
+          style={{ color: "#F26D21", fontWeight: "bold", textAlign: "center" }}
+        >
+          Thông báo
+        </h1>
+        <p style={{ textAlign: "center" }}>Bạn đã nộp báo cáo thành công.</p>
+
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Button
+            onClick={handleOk}
+            style={{ background: "#EDEDED", padding: "20px" }}
+          >
+            Đóng
+          </Button>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default FormLearningProcess;
--- src/pages/LearningProcess/index.tsx
+++ src/pages/LearningProcess/index.tsx
@@ -3,11 +3,14 @@ import { RootState } from "@/redux/store";
 import React, { useEffect } from "react";
 
 import { useNavigate } from "react-router-dom";
+import FormLearningProcess from "@/components/FormLearningProcess";
 
 const LearningProcess: React.FC = () => {
   const isAuthenticated = useSelector(
     (state: RootState) => state.auth.isAuthenticated
   );
+    const user = useSelector((state: RootState) => state.auth);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,18 +20,24 @@ const LearningProcess: React.FC = () => {
   }, [isAuthenticated, navigate]);
   return (
     <>
-      <div style={{ margin: "50px 0px" }}>
-        <h1
-          style={{
-            maxWidth: 400,
-            margin: "30px auto",
-            textAlign: "center",
-            color: "rgb(255, 90, 0)",
-            fontWeight: "bold",
-          }}
-        >
-          TIẾN TRÌNH HỌC TẬP
-        </h1>
+      <div style={{ margin: "50px 50px" }}>
+        {isAuthenticated && user ? (
+          <>
+            <h1
+              style={{
+                margin: "30px 20px",
+
+                color: "rgb(255, 90, 0)",
+                fontWeight: "bold",
+              }}
+            >
+              <span style={{ color: "black" }}>Xin chào</span> {user.name}
+            </h1>
+          </>
+        ) : (
+          <></>
+        )}
+        <FormLearningProcess/>  
       </div>
     </>
   );
